d39378ed5df2d8a67852d8f384442e62b6719141
ongaeshi <ongaeshi0621@gmail.com>
2019-10-29 02:00:05 +0900

    style: Rename to ruby style

--- models_box_collisions.rb
+++ models_box_collisions.rb
@@ -10,15 +10,15 @@ window(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [models] example - box collisions") 
   # Define the camera to look into our 3d world
   camera = Camera3D.init(Vector3.init(0.0, 10.0, 10.0), Vector3.init(0.0, 0.0, 0.0), Vector3.init(0.0, 1.0, 0.0), 45.0, 0)
 
-  playerPosition = Vector3.init(0.0, 1.0, 2.0)
-  playerSize = Vector3.init(1.0, 2.0, 1.0)
-  playerColor = GREEN
+  player_position = Vector3.init(0.0, 1.0, 2.0)
+  player_size = Vector3.init(1.0, 2.0, 1.0)
+  player_color = GREEN
 
-  enemyBoxPos = Vector3.init(-4.0, 1.0, 0.0)
-  enemyBoxSize = Vector3.init(2.0, 2.0, 2.0)
+  enemy_box_pos = Vector3.init(-4.0, 1.0, 0.0)
+  enemy_box_size = Vector3.init(2.0, 2.0, 2.0)
 
-  enemySpherePos = Vector3.init(4.0, 0.0, 0.0)
-  enemySphereSize = 1.5
+  enemy_sphere_pos = Vector3.init(4.0, 0.0, 0.0)
+  enemy_sphere_size = 1.5
 
   collision = false;
 
@@ -30,51 +30,51 @@ window(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [models] example - box collisions") 
 
     # Move player
     if is_key_down(KEY_RIGHT)
-      playerPosition.x += 0.2
+      player_position.x += 0.2
     elsif is_key_down(KEY_LEFT)
-      playerPosition.x -= 0.2
+      player_position.x -= 0.2
     elsif is_key_down(KEY_DOWN)
-      playerPosition.z += 0.2
+      player_position.z += 0.2
     elsif is_key_down(KEY_UP)
-      playerPosition.z -= 0.2
+      player_position.z -= 0.2
     end
 
     collision = false;
 
     # Check collisions player vs enemy-box
     if check_collision_boxes(
-      BoundingBox.init(Vector3.init(playerPosition.x - playerSize.x/2,
-                                    playerPosition.y - playerSize.y/2,
-                                    playerPosition.z - playerSize.z/2),
-                      Vector3.init(playerPosition.x + playerSize.x/2,
-                                    playerPosition.y + playerSize.y/2,
-                                    playerPosition.z + playerSize.z/2)),
-      BoundingBox.init(Vector3.init(enemyBoxPos.x - enemyBoxSize.x/2,
-                                    enemyBoxPos.y - enemyBoxSize.y/2,
-                                    enemyBoxPos.z - enemyBoxSize.z/2),
-                      Vector3.init(enemyBoxPos.x + enemyBoxSize.x/2,
-                                    enemyBoxPos.y + enemyBoxSize.y/2,
-                                    enemyBoxPos.z + enemyBoxSize.z/2))
+      BoundingBox.init(Vector3.init(player_position.x - player_size.x/2,
+                                    player_position.y - player_size.y/2,
+                                    player_position.z - player_size.z/2),
+                      Vector3.init(player_position.x + player_size.x/2,
+                                    player_position.y + player_size.y/2,
+                                    player_position.z + player_size.z/2)),
+      BoundingBox.init(Vector3.init(enemy_box_pos.x - enemy_box_size.x/2,
+                                    enemy_box_pos.y - enemy_box_size.y/2,
+                                    enemy_box_pos.z - enemy_box_size.z/2),
+                      Vector3.init(enemy_box_pos.x + enemy_box_size.x/2,
+                                    enemy_box_pos.y + enemy_box_size.y/2,
+                                    enemy_box_pos.z + enemy_box_size.z/2))
                                     )
       collision = true
     end
 
     # Check collisions player vs enemy-sphere
     if check_collision_box_sphere(
-      BoundingBox.init(Vector3.init(playerPosition.x - playerSize.x/2,
-                                    playerPosition.y - playerSize.y/2,
-                                    playerPosition.z - playerSize.z/2),
-                      Vector3.init(playerPosition.x + playerSize.x/2,
-                                    playerPosition.y + playerSize.y/2,
-                                    playerPosition.z + playerSize.z/2)),
-                enemySpherePos, enemySphereSize)
+      BoundingBox.init(Vector3.init(player_position.x - player_size.x/2,
+                                    player_position.y - player_size.y/2,
+                                    player_position.z - player_size.z/2),
+                      Vector3.init(player_position.x + player_size.x/2,
+                                    player_position.y + player_size.y/2,
+                                    player_position.z + player_size.z/2)),
+                enemy_sphere_pos, enemy_sphere_size)
       collision = true
     end
 
     if (collision)
-      playerColor = RED 
+      player_color = RED 
     else
-      playerColor = GREEN
+      player_color = GREEN
     end
 
     # Draw
@@ -83,15 +83,15 @@ window(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [models] example - box collisions") 
 
       mode3d(camera) do
         # Draw enemy-box
-        draw_cube(enemyBoxPos, enemyBoxSize.x, enemyBoxSize.y, enemyBoxSize.z, GRAY)
-        draw_cube_wires(enemyBoxPos, enemyBoxSize.x, enemyBoxSize.y, enemyBoxSize.z, DARKGRAY)
+        draw_cube(enemy_box_pos, enemy_box_size.x, enemy_box_size.y, enemy_box_size.z, GRAY)
+        draw_cube_wires(enemy_box_pos, enemy_box_size.x, enemy_box_size.y, enemy_box_size.z, DARKGRAY)
 
         # Draw enemy-sphere
-        draw_sphere(enemySpherePos, enemySphereSize, GRAY)
-        draw_sphere_wires(enemySpherePos, enemySphereSize, 16, 16, DARKGRAY)
+        draw_sphere(enemy_sphere_pos, enemy_sphere_size, GRAY)
+        draw_sphere_wires(enemy_sphere_pos, enemy_sphere_size, 16, 16, DARKGRAY)
 
         # Draw player
-        draw_cube_v(playerPosition, playerSize, playerColor)
+        draw_cube_v(player_position, player_size, player_color)
 
         draw_grid(10, 1.0)        # Draw a grid
       end
