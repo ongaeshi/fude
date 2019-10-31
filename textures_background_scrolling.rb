0a32aa2ee60bb968a49414c3c23cb5c581daee0a
ongaeshi <ongaeshi0621@gmail.com>
2019-11-01 00:11:47 +0900

    refactor: ruby style

--- textures_background_scrolling.rb
+++ textures_background_scrolling.rb
@@ -2,70 +2,59 @@
 require './raylib'
 include Raylib
 
-    # Initialization
-    screenWidth = 800;
-    screenHeight = 450;
+# Initialization
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 450
 
-    init_window(screenWidth, screenHeight, "raylib [textures] example - background scrolling");
+window(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [textures] example - background scrolling") do
+  # NOTE: Be careful, background width must be equal or bigger than screen width
+  # if not, texture should be draw more than two times for scrolling effect
+  background = load_texture("resources/cyberpunk_street_background.png")
+  midground = load_texture("resources/cyberpunk_street_midground.png")
+  foreground = load_texture("resources/cyberpunk_street_foreground.png")
 
-    # NOTE: Be careful, background width must be equal or bigger than screen width
-    # if not, texture should be draw more than two times for scrolling effect
-    background = load_texture("resources/cyberpunk_street_background.png");
-    midground = load_texture("resources/cyberpunk_street_midground.png");
-    foreground = load_texture("resources/cyberpunk_street_foreground.png");
+  scrolling_back = 0.0
+  scrolling_mid = 0.0
+  scrolling_fore = 0.0
 
-    scrollingBack = 0.0;
-    scrollingMid = 0.0;
-    scrollingFore = 0.0;
+  set_target_fps(60)               # Set our game to run at 60 frames-per-second
 
-    set_target_fps(60);               # Set our game to run at 60 frames-per-second
-    #--------------------------------------------------------------------------------------
+  # Main game loop
+  until window_should_close()    # Detect window close button or ESC key
+    # Update
+    scrolling_back -= 0.1
+    scrolling_mid -= 0.5
+    scrolling_fore -= 1.0
 
-    # Main game loop
-    while (!window_should_close())    # Detect window close button or ESC key
-        # Update
-        #----------------------------------------------------------------------------------
-        scrollingBack -= 0.1;
-        scrollingMid -= 0.5;
-        scrollingFore -= 1.0;
+    # NOTE: Texture is scaled twice its size, so it sould be considered on scrolling
+    scrolling_back = 0 if (scrolling_back <= -background.width*2)
+    scrolling_mid = 0 if (scrolling_mid <= -midground.width*2)
+    scrolling_fore = 0 if (scrolling_fore <= -foreground.width*2)
 
-        # NOTE: Texture is scaled twice its size, so it sould be considered on scrolling
-        scrollingBack = 0 if (scrollingBack <= -background.width*2)
-        scrollingMid = 0 if (scrollingMid <= -midground.width*2)
-        scrollingFore = 0 if (scrollingFore <= -foreground.width*2)
-        #----------------------------------------------------------------------------------
+    # Draw
+    draw do
+      clear_background(get_color(0x052c46ff))
 
-        # Draw
-        #----------------------------------------------------------------------------------
-        begin_drawing();
+      # Draw background image twice
+      # NOTE: Texture is scaled twice its size
+      draw_texture_ex(background, Vector2.init(scrolling_back, 20), 0.0, 2.0, WHITE)
+      draw_texture_ex(background, Vector2.init(background.width*2 + scrolling_back, 20), 0.0, 2.0, WHITE)
 
-            clear_background(get_color(0x052c46ff));
+      # Draw midground image twice
+      draw_texture_ex(midground, Vector2.init(scrolling_mid, 20), 0.0, 2.0, WHITE)
+      draw_texture_ex(midground, Vector2.init(midground.width*2 + scrolling_mid, 20), 0.0, 2.0, WHITE)
 
-            # Draw background image twice
-            # NOTE: Texture is scaled twice its size
-            draw_texture_ex(background, Vector2.init(scrollingBack, 20), 0.0, 2.0, WHITE);
-            draw_texture_ex(background, Vector2.init(background.width*2 + scrollingBack, 20), 0.0, 2.0, WHITE);
+      # Draw foreground image twice
+      draw_texture_ex(foreground, Vector2.init(scrolling_fore, 70), 0.0, 2.0, WHITE)
+      draw_texture_ex(foreground, Vector2.init(foreground.width*2 + scrolling_fore, 70), 0.0, 2.0, WHITE)
 
-            # Draw midground image twice
-            draw_texture_ex(midground, Vector2.init(scrollingMid, 20), 0.0, 2.0, WHITE);
-            draw_texture_ex(midground, Vector2.init(midground.width*2 + scrollingMid, 20), 0.0, 2.0, WHITE);
-
-            # Draw foreground image twice
-            draw_texture_ex(foreground, Vector2.init(scrollingFore, 70), 0.0, 2.0, WHITE);
-            draw_texture_ex(foreground, Vector2.init(foreground.width*2 + scrollingFore, 70), 0.0, 2.0, WHITE);
-
-            draw_text("BACKGROUND SCROLLING & PARALLAX", 10, 10, 20, RED);
-            draw_text("(c) Cyberpunk Street Environment by Luis Zuno (@ansimuz)", screenWidth - 330, screenHeight - 20, 10, RAYWHITE);
-
-        end_drawing();
-        #----------------------------------------------------------------------------------
+      draw_text("BACKGROUND SCROLLING & PARALLAX", 10, 10, 20, RED)
+      draw_text("(c) Cyberpunk Street Environment by Luis Zuno (@ansimuz)", SCREEN_WIDTH - 330, SCREEN_HEIGHT - 20, 10, RAYWHITE)
     end
+  end
 
-    # De-Initialization
-    #--------------------------------------------------------------------------------------
-    unload_texture(background);  # Unload background texture
-    unload_texture(midground);   # Unload midground texture
-    unload_texture(foreground);  # Unload foreground texture
-
-    close_window();              # Close window and OpenGL context
-    #--------------------------------------------------------------------------------------
+  # De-Initialization
+  unload_texture(background)  # Unload background texture
+  unload_texture(midground)   # Unload midground texture
+  unload_texture(foreground)  # Unload foreground texture
+end
